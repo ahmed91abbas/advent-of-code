910bd4267e48228348f73fe761816c1ab0157278
Ahmed Abbas <ahmed.abbas1@ingka.ikea.com>
2021-12-02 08:13:40 +0100

    finish day 2

---
+++ day02/1.py
@@ -0,0 +1,14 @@
+with open('data.in') as f:
+    lines = f.read().splitlines()
+
+directions = {
+    'forward': 0,
+    'down': 0,
+    'up': 0
+}
+for line in lines:
+    key, value = line.split(' ')
+    directions[key] += int(value)
+
+result = directions['forward'] * (directions['down'] - directions['up'])
+print(result)
---
+++ day02/2.py
@@ -0,0 +1,20 @@
+with open('data.in') as f:
+    lines = f.read().splitlines()
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for line in lines:
+    key, value = line.split(' ')
+    value = int(value)
+    if key == 'forward':
+        horizontal += value
+        depth += (aim * value)
+    elif key == 'down':
+        aim += value
+    elif key == 'up':
+        aim -= value
+
+result = horizontal * depth
+print(result)
