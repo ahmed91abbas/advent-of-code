9b7e3cbddacd938938c015011dccd977c0279ca2
Ahmed Abbas <ahmed.abbas1@ingka.ikea.com>
2021-12-10 08:37:22 +0100

    finish day 10

---
+++ 2021/day10/1.py
@@ -0,0 +1,33 @@
+with open('data.in') as f:
+    lines = f.read().splitlines()
+
+closing_scores = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
+
+opening_syntax = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>'
+}
+
+def get_first_illegal_char(line):
+    opening = list()
+    for c in line:
+        if c in opening_syntax:
+            opening.append(c)
+        else:
+            last = opening.pop()
+            if c != opening_syntax[last]:
+                return c
+    return ''
+
+result = 0
+for line in lines:
+    result += closing_scores.get(get_first_illegal_char(line), 0)
+
+print(result)
---
+++ 2021/day10/2.py
@@ -0,0 +1,54 @@
+with open('data.in') as f:
+    lines = f.read().splitlines()
+
+closing_scores = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+
+opening_syntax = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>'
+}
+
+def is_valid(line):
+    opening = list()
+    for c in line:
+        if c in opening_syntax:
+            opening.append(c)
+        else:
+            last = opening.pop()
+            if c != opening_syntax[last]:
+                return False
+    return True
+
+def get_missing_syntax(line):
+    missing = ''
+    opening = list()
+    for c in line:
+        if c in opening_syntax:
+            opening.append(c)
+        else:
+            opening.pop()
+    while opening:
+        last = opening.pop()
+        missing += opening_syntax[last]
+    return missing
+
+def get_score(line):
+    score = 0
+    for c in line:
+        score = score * 5 + closing_scores[c]
+    return score
+
+scores = list()
+valid_lines = [line for line in lines if is_valid(line)]
+for line in valid_lines:
+    missing_syntax = get_missing_syntax(line)
+    scores.append(get_score(missing_syntax))
+
+print(sorted(scores)[len(scores)//2])
