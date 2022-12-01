40035390074dc3aa92e0f74673e2b9004d4e9ff8
ahmed91abbas <ahmed91abbas@hotmail.com>
2022-12-01 20:01:52 +0100

    finish day 01

---
+++ 2022/day01/2.py
@@ -0,0 +1,13 @@
+with open("data.in", "r") as f:
+    lines = f.read()
+values = lines.split("\n")
+current = 0
+sums = []
+for x in values:
+    if x:
+        current += int(x)
+    else:
+        sums.append(current)
+        current = 0
+
+print(sum(sorted(sums, reverse=True)[:3]))
