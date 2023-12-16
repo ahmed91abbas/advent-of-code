fcaa3ad4b0679c0decb855d79263f391c32adcd5
ahmed91abbas <ahmed91abbas@hotmail.com>
2023-12-16 09:51:01 +0100

    day 16 - 2023

---
+++ 2023/day16/1.py
@@ -0,0 +1,107 @@
+with open("data.in", "r") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+
+"""
+---1>-------2<--------> x
+|
+3
+v
+|
+|
+4
+n
+|
+|
+v
+y
+
+1 = (1, 0)
+2 = (-1, 0)
+3 = (0, 1)
+4 = (0, -1)
+"""
+
+
+class Beam:
+    def __init__(self, x, y, max_x, max_y, dir):
+        self.x = x
+        self.y = y
+        self.max_x = max_x
+        self.max_y = max_y
+        self.dir = dir
+
+    def move(self):
+        self.x += self.dir[0]
+        self.y += self.dir[1]
+
+    def split(self, tile):
+        if tile == ".":
+            return self, None
+        if tile == "|":
+            if self.dir[0] == 0:
+                return self, None
+            if self.dir[1] == 0:
+                self.dir = (0, 1)
+                return self, Beam(self.x, self.y, self.max_x, self.max_y, (0, -1))
+        if tile == "-":
+            if self.dir[0] == 0:
+                self.dir = (1, 0)
+                return self, Beam(self.x, self.y, self.max_x, self.max_y, (-1, 0))
+            if self.dir[1] == 0:
+                return self, None
+        if tile == "/":
+            if self.dir == (1, 0):
+                self.dir = (0, -1)
+                return self, None
+            if self.dir == (-1, 0):
+                self.dir = (0, 1)
+                return self, None
+            if self.dir == (0, 1):
+                self.dir = (-1, 0)
+                return self, None
+            if self.dir == (0, -1):
+                self.dir = (1, 0)
+                return self, None
+        if tile == "\\":
+            if self.dir == (1, 0):
+                self.dir = (0, 1)
+                return self, None
+            if self.dir == (-1, 0):
+                self.dir = (0, -1)
+                return self, None
+            if self.dir == (0, 1):
+                self.dir = (1, 0)
+                return self, None
+            if self.dir == (0, -1):
+                self.dir = (-1, 0)
+                return self, None
+
+    def finished(self):
+        return self.x < 0 or self.y < 0 or self.x >= self.max_x or self.y >= self.max_y
+
+    @property
+    def key(self):
+        return f"{self.x}, {self.y}, {self.dir}"
+
+
+graph = []
+for line in lines:
+    graph.append(list(line))
+
+start_beam = Beam(0, 0, len(graph[0]), len(graph), (1, 0))
+beams = {start_beam}
+energized = set()
+handled = set()
+while beams:
+    current_beam = beams.pop()
+    while not current_beam.finished() and current_beam.key not in handled:
+        energized.add((current_beam.x, current_beam.y))
+        handled.add(current_beam.key)
+        tile = graph[current_beam.y][current_beam.x]
+        current_beam, new_beam = current_beam.split(tile)
+        if new_beam:
+            beams.add(new_beam)
+        current_beam.move()
+
+print(len(energized))
